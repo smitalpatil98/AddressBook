beba51265969a9409e5aa2c4d954ab3c747a7b67
smitalpatil98 <patilsmital.1101998gmail.com>
2024-05-30 12:18:30 +0530

    UC_3_Edit_Contacts

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -10,8 +10,8 @@ public class AddressBook
     String phoneNumber;
     String email;
 
-    public AddressBook(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email)
-    {
+
+    public AddressBook(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
@@ -21,35 +21,8 @@ public class AddressBook
         this.phoneNumber = phoneNumber;
         this.email = email;
     }
+
     public static void main(String[] args)
     {
-        ArrayList<String> Contact = new ArrayList<String>();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter First Name");
-        String first_name = sc.nextLine();
-        Contact.add(first_name);
-        System.out.println("Enter Last name");
-        String last_name = sc.nextLine();
-        Contact.add(last_name);
-        System.out.println("Enter Address");
-        String address = sc.nextLine();
-        Contact.add(address);
-        System.out.println("Enter City");
-        String city = sc.nextLine();
-        Contact.add(city);
-        System.out.println("Enter State");
-        String state = sc.nextLine();
-        Contact.add(state);
-        System.out.println("Enter ZIp");
-        String zip = sc.nextLine();
-        Contact.add(zip);
-        System.out.println("Enter Phone number");
-        String ph_number = sc.nextLine();
-        Contact.add(ph_number);
-        System.out.println("Enter Email");
-        String email = sc.nextLine();
-        Contact.add(email);
-        for (String s : Contact) {
-            System.out.println(s);
-        }
-}}
+    }
+}
--- src/Contact.java
+++ src/Contact.java
@@ -1,30 +1,124 @@
-
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Contact
 {
-    String firstName;
-    String lastName;
-    String address;
-    String city;
-    String state;
-    String zip;
-    String phoneNumber;
-    String email;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private String phoneNumber;
+    private String email;
 
-    public Contact(String firstName,String lastName,String address,String city,String state,String phoneNumber, String email)
+    private static HashMap<String, String> person = new HashMap<>();
+
+    public Contact()
     {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.city = city;
-        this.zip = zip;
-        this.phoneNumber = phoneNumber;
-        this.email =email;
     }
-    public String getInfo()
+
+    public void addContact()
     {
-        return "First name is"+firstName+"Last Name is"+lastName+"Address is"+address+"City is"+city+"State is"+state+"Zip is "+zip+"Phone number is"+phoneNumber+"Email is "+email;
+        Scanner sc = new Scanner(System.in);
 
-    }
+        System.out.println("Enter First Name");
+        String first_name = sc.nextLine();
+        person.put("firstName", first_name);
+
+        System.out.println("Enter Last Name");
+        String last_name = sc.nextLine();
+        person.put("lastName", last_name);
+
+        System.out.println("Enter Address");
+        String address = sc.nextLine();
+        person.put("address", address);
+
+        System.out.println("Enter City");
+        String city = sc.nextLine();
+        person.put("city", city);
+
+        System.out.println("Enter State");
+        String state = sc.nextLine();
+        person.put("state", state);
+
+        System.out.println("Enter Zip");
+        String zip = sc.nextLine();
+        person.put("zip", zip);
+
+        System.out.println("Enter Phone Number");
+        String phoneNumber = sc.nextLine();
+        person.put("phoneNumber", phoneNumber);
+
+        System.out.println("Enter Email");
+        String email = sc.nextLine();
+        person.put("email", email);
     }
 
+    public void updateContact()
+    {
+        System.out.println("Enter Value to Change\n(1) First name\n(2) Last name\n(3) Address\n(4) City\n(5) State\n(6) Zip\n(7) Phone Number\n(8) Email");
+        Scanner sc = new Scanner(System.in);
+        int option = sc.nextInt();
+        sc.nextLine();  // Consume newline left-over
+        String newValue;
+
+        switch (option)
+        {
+            case 1:
+                System.out.println("Enter new First name:");
+                newValue = sc.nextLine();
+                person.replace("firstName", newValue);
+                break;
+            case 2:
+                System.out.println("Enter new Last name:");
+                newValue = sc.nextLine();
+                person.replace("lastName", newValue);
+                break;
+            case 3:
+                System.out.println("Enter new Address:");
+                newValue = sc.nextLine();
+                person.replace("address", newValue);
+                break;
+            case 4:
+                System.out.println("Enter new City:");
+                newValue = sc.nextLine();
+                person.replace("city", newValue);
+                break;
+            case 5:
+                System.out.println("Enter new State:");
+                newValue = sc.nextLine();
+                person.replace("state", newValue);
+                break;
+            case 6:
+                System.out.println("Enter new Zip:");
+                newValue = sc.nextLine();
+                person.replace("zip", newValue);
+                break;
+            case 7:
+                System.out.println("Enter new Phone Number:");
+                newValue = sc.nextLine();
+                person.replace("phoneNumber", newValue);
+                break;
+            case 8:
+                System.out.println("Enter new Email:");
+                newValue = sc.nextLine();
+                person.replace("email", newValue);
+                break;
+            default:
+                System.out.println("Invalid option.");
+                break;
+        }
+    }
+
+
+    public String getInfo() {
+        System.out.println("Updated Contact Information:");
+        StringBuilder output = new StringBuilder();
+        for (Map.Entry<String, String> entry : person.entrySet()) {
+            output.append(entry.getKey()).append(" is: ").append(entry.getValue()).append("\n");
+        }
+        return output.toString();
+    }
+}
