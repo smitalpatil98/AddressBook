a06fda2606b78681520abcfdaa298f8bdd79aae4
smitalpatil98 <patilsmital.1101998gmail.com>
2024-06-17 14:51:34 +0530

    UC_5

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,40 +1,100 @@
+//Refactor to add multiple Address Book to the System. Each Address Book has a unique Name -
+// Use Console to add new Address Book - Maintain Dictionary of Address Book Name to Address Book
 import java.util.*;
 import java.util.Scanner;
-public class AddressBook {
-    public static boolean exit = true;
+public class AddressBook
+{
+    private HashMap<String, Contact> contacts;
 
-    public static void options() {
-        System.out.println("(1) Create New Contact\n(2) Edit Contact\n(3) Delete Contact\n(4) Display\n(5) Exit");
-        Scanner sc = new Scanner(System.in);
-        Contact obj = new Contact();
-        int option = sc.nextInt();
+    public AddressBook() {
+        contacts = new HashMap<>();
+    }
 
-        switch (option) {
-            case 1:
-                obj.addContact();
-                break;
-            case 2:
-                obj.updateContact();
-                break;
-            case 3:
-                obj.deleteInfo();
-                break;
-            case 4:
-                System.out.println(obj.getInfo());
-                break;
-            case 5:
-                exit = false;
-                break;
-            default:
-                System.out.println("Invalid option.");
-                break;
+    public void addContact(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email) {
+        Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+        contacts.put(firstName, contact);
+    }
+
+    public Contact getContact(String name) {
+        return contacts.get(name);
+    }
+
+    public void removeContact(String name) {
+        contacts.remove(name);
+    }
+
+    public void listContacts() {
+        for (Map.Entry<String, Contact> entry : contacts.entrySet()) {
+            System.out.println(entry.getValue());
         }
     }
 
     public static void main(String[] args) {
-        System.out.println("******Welcome to Address Book*******");
-        while (exit) {
-            options();
+        Scanner scanner = new Scanner(System.in);
+        AddressBook addressBook = new AddressBook();
+        boolean exit = false;
+
+        while (!exit) {
+            System.out.println("\nAddress Book Menu:");
+            System.out.println("1. Add Contact");
+            System.out.println("2. Retrieve Contact");
+            System.out.println("3. Remove Contact");
+            System.out.println("4. List Contacts");
+            System.out.println("5. Exit");
+            System.out.print("Choose an option: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // consume newline
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter first name: ");
+                    String firstName = scanner.nextLine();
+                    System.out.print("Enter last name: ");
+                    String lastName = scanner.nextLine();
+                    System.out.print("Enter address: ");
+                    String address = scanner.nextLine();
+                    System.out.print("Enter city: ");
+                    String city = scanner.nextLine();
+                    System.out.print("Enter state: ");
+                    String state = scanner.nextLine();
+                    System.out.print("Enter zip code: ");
+                    String zip = scanner.nextLine();
+                    System.out.print("Enter phone number: ");
+                    String phoneNumber = scanner.nextLine();
+                    System.out.print("Enter email: ");
+                    String email = scanner.nextLine();
+                    addressBook.addContact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+                    System.out.println("Contact added.");
+                    break;
+                case 2:
+                    System.out.print("Enter first name to retrieve: ");
+                    String nameToRetrieve = scanner.nextLine();
+                    Contact contact = addressBook.getContact(nameToRetrieve);
+                    if (contact != null) {
+                        System.out.println("Contact found: " + contact);
+                    } else {
+                        System.out.println("Contact not found.");
+                    }
+                    break;
+                case 3:
+                    System.out.print("Enter first name to remove: ");
+                    String nameToRemove = scanner.nextLine();
+                    addressBook.removeContact(nameToRemove);
+                    System.out.println("Contact removed if it existed.");
+                    break;
+                case 4:
+                    System.out.println("Listing all contacts:");
+                    addressBook.listContacts();
+                    break;
+                case 5:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
         }
+
+        scanner.close();
     }
 }
--- src/Contact.java
+++ src/Contact.java
@@ -2,162 +2,40 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class Contact {
-    private String firstName;
-    private String lastName;
-    private String address;
-    private String city;
-    private String state;
-    private String zip;
-    private String phoneNumber;
-    private String email;
+public class Contact
+{
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    String zip;
+    String phNumber;
+    String email;
+    String contactId;
 
-    private static HashMap<String, String> person = new HashMap<>();
+    public Contact(String firstName,String lastName,String address,String city,String state,String zip,String phNumber,String email ){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.address=address;
+        this.city=city;
+        this.state=state;
+        this.zip=zip;
+        this.phNumber=phNumber;
+        this.email=email;
 
-    public Contact() {
     }
 
-    public void addContact() {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter First Name");
-        String first_name = sc.nextLine();
-        person.put("firstName", first_name);
-
-        System.out.println("Enter Last Name");
-        String last_name = sc.nextLine();
-        person.put("lastName", last_name);
-
-        System.out.println("Enter Address");
-        String address = sc.nextLine();
-        person.put("address", address);
-
-        System.out.println("Enter City");
-        String city = sc.nextLine();
-        person.put("city", city);
-
-        System.out.println("Enter State");
-        String state = sc.nextLine();
-        person.put("state", state);
-
-        System.out.println("Enter Zip");
-        String zip = sc.nextLine();
-        person.put("zip", zip);
-
-        System.out.println("Enter Phone Number");
-        String phoneNumber = sc.nextLine();
-        person.put("phoneNumber", phoneNumber);
-
-        System.out.println("Enter Email");
-        String email = sc.nextLine();
-        person.put("email", email);
-    }
-
-    public void updateContact() {
-        System.out.println("Enter Value to Change\n(1) First name\n(2) Last name\n(3) Address\n(4) City\n(5) State\n(6) Zip\n(7) Phone Number\n(8) Email");
-        Scanner sc = new Scanner(System.in);
-        int option = sc.nextInt();
-        sc.nextLine();  // Consume newline left-over
-        String newValue;
-
-        switch (option) {
-            case 1:
-                System.out.println("Enter new First name:");
-                newValue = sc.nextLine();
-                person.replace("firstName", newValue);
-                break;
-            case 2:
-                System.out.println("Enter new Last name:");
-                newValue = sc.nextLine();
-                person.replace("lastName", newValue);
-                break;
-            case 3:
-                System.out.println("Enter new Address:");
-                newValue = sc.nextLine();
-                person.replace("address", newValue);
-                break;
-            case 4:
-                System.out.println("Enter new City:");
-                newValue = sc.nextLine();
-                person.replace("city", newValue);
-                break;
-            case 5:
-                System.out.println("Enter new State:");
-                newValue = sc.nextLine();
-                person.replace("state", newValue);
-                break;
-            case 6:
-                System.out.println("Enter new Zip:");
-                newValue = sc.nextLine();
-                person.replace("zip", newValue);
-                break;
-            case 7:
-                System.out.println("Enter new Phone Number:");
-                newValue = sc.nextLine();
-                person.replace("phoneNumber", newValue);
-                break;
-            case 8:
-                System.out.println("Enter new Email:");
-                newValue = sc.nextLine();
-                person.replace("email", newValue);
-                break;
-            default:
-                System.out.println("Invalid option.");
-                break;
-        }
-    }
-
-    public void deleteInfo() {
-        System.out.println("Enter the field to delete\n(1) First name\n(2) Last name\n(3) Address\n(4) City\n(5) State\n(6) Zip\n(7) Phone Number\n(8) Email");
-        Scanner sc = new Scanner(System.in);
-        int option = sc.nextInt();
-        sc.nextLine();  // Consume newline left-over
-
-        switch (option) {
-            case 1:
-                person.remove("firstName");
-                System.out.println("First name removed.");
-                break;
-            case 2:
-                person.remove("lastName");
-                System.out.println("Last name removed.");
-                break;
-            case 3:
-                person.remove("address");
-                System.out.println("Address removed.");
-                break;
-            case 4:
-                person.remove("city");
-                System.out.println("City removed.");
-                break;
-            case 5:
-                person.remove("state");
-                System.out.println("State removed.");
-                break;
-            case 6:
-                person.remove("zip");
-                System.out.println("Zip removed.");
-                break;
-            case 7:
-                person.remove("phoneNumber");
-                System.out.println("Phone number removed.");
-                break;
-            case 8:
-                person.remove("email");
-                System.out.println("Email removed.");
-                break;
-            default:
-                System.out.println("Invalid option.");
-                break;
-        }
-    }
-
-    public String getInfo() {
-        System.out.println("Updated Contact Information:");
-        StringBuilder output = new StringBuilder();
-        for (Map.Entry<String, String> entry : person.entrySet()) {
-            output.append(entry.getKey()).append(" is: ").append(entry.getValue()).append("\n");
-        }
-        return output.toString();
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "  Name= '" + firstName +" "+lastName+  '\'' +
+                ", PhoneNumber= '" + phNumber + '\'' +
+                ", Email= '" + email + '\'' +
+                ", Address= '"+address + '\'' +
+                ", City= '"+ city +'\'' +
+                ", State= '"+ state + '\'' +
+                ", Zip= '"+ zip + '\'' +
+                '}';
     }
 }
